d0ec3966557bb31ac6c667dee54faacb2e988a09
K98066 <146914490+K98066@users.noreply.github.com>
2023-12-12 16:03:06 +0800

    Update binary_tree_college.cpp

--- binary_tree_college.cpp
+++ binary_tree_college.cpp
@@ -60,17 +60,17 @@ void IDsortTree(Tree *&tree, Tree *add) {
         tree = add;
         return;
     }
-
+    
     Tree *temp = tree;
     while (true) {
-        if (add->college.schoolID < temp->college.schoolID) {
+        if (add->college.graduate_count < temp->college.graduate_count) {
             if (temp->left_college == NULL) {
                 temp->left_college = add;
                 break;
             } else {
                 temp = temp->left_college;
             }
-        } else if (add->college.schoolID > temp->college.schoolID) {
+        } else if (add->college.graduate_count > temp->college.graduate_count) {
             if (temp->right_college == NULL) {
                 temp->right_college = add;
                 break;
@@ -83,32 +83,33 @@ void IDsortTree(Tree *&tree, Tree *add) {
     }
 }
 
-void NAMEsortTree(Tree *&tree, Tree *add)
-{
-    if (tree == NULL)
-    {
+
+void NamesortTree(Tree *&tree, Tree *add) {
+    if (tree == NULL) {
         tree = add;
         return;
     }
-
+    
     Tree *temp = tree;
-    int dir = 0;
-
-    while (temp != NULL)
-    {
-        if (strcmp(add->college.school_name, tree->college.school_name) < 0)
-        {
-            dir = 1; // add to left
-            temp = temp->left_college;
-        }
-        else if (strcmp(add->college.school_name, tree->college.school_name) > 0)
-        {
-            dir = 2; // add to right
-            temp = temp->right_college;
+    while (true) {
+        if (add->college.school_name < tree->college.school_name) {
+            if (temp->left_college == NULL) {
+                temp->left_college = add;
+                break;
+            } else {
+                temp = temp->left_college;
+            }
+        } else if (add->college.school_name > tree->college.school_name) {
+            if (temp->right_college == NULL) {
+                temp->right_college = add;
+                break;
+            } else {
+                temp = temp->right_college;
+            }
+        } else {
+            break;
         }
     }
-
-    addTree(temp, add, dir);
 }
 
 struct Data
@@ -130,19 +131,19 @@ void readData(Data *&data, const char *filename) {
         return;
     }
 
-    data = nullptr;
-    Data* temp = nullptr;
+    data = NULL;
+    Data* temp = NULL;
     
     while (true) {
         Data* newData = new Data;
-        newData->next = nullptr;
+        newData->next = NULL;
         
         if (infile >> newData->college.schoolID >> newData->college.school_name >> newData->college.subjectID >> newData->college.subject_name
             >> newData->college.day_or_night_e >> newData->college.day_or_night_c >> newData->college.level_e >> newData->college.level_c
             >> newData->college.student_count >> newData->college.teacher_count  >> newData->college.graduate_count >> newData->college.city_n
             >> newData->college.city_c >> newData->college.system_n >> newData->college.system_c) {
             
-            if (data == nullptr) {
+            if (data == NULL) {
                 data = newData;
                 temp = data;
             } else {
@@ -165,22 +166,48 @@ void printInOrder(Tree* tree) {
     }
 }
 
+int calculateTreeHeight(Tree* tree) {
+    if (tree == NULL) {
+        return 0;
+    } else {
+        int leftHeight = calculateTreeHeight(tree->left_college);
+        int rightHeight = calculateTreeHeight(tree->right_college);
+
+        return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+    }
+}
+
+
 int main()
 {
     Data *all_data = NULL;
-    if (all_data == NULL) readData(all_data, "input401.txt");
+    if (all_data == NULL) readData(all_data, "input.txt");
 
-    Tree *tree_top = NULL;
-    Tree *add_tree = new Tree;
+	Tree *n_tree_top = NULL;
+    Tree *g_tree_top = NULL;
     Data *temp = all_data;
 
     while (temp != NULL)
     {
+    	Tree *add_tree = new Tree;
         add_tree->college = temp->college;
-        IDsortTree(tree_top, add_tree);
-        printf("%d\n", tree_top->college.schoolID);
+        IDsortTree(g_tree_top, add_tree);
         temp = temp->next;
     }
 
+	temp = all_data;
+	
+	while (temp != NULL) {
+    	Tree *add_tree = new Tree;
+        add_tree->college = temp->college;
+        NamesortTree(n_tree_top, add_tree);
+        temp = temp->next;
+	}
+
+	int g_treeHeight = calculateTreeHeight(g_tree_top);
+    cout << "Binary Tree Height: " << g_treeHeight << endl;
+    int n_treeHeight = calculateTreeHeight(n_tree_top);
+    cout << "Binary Tree Height: " << n_treeHeight << endl;
+	
     return 0;
 }
